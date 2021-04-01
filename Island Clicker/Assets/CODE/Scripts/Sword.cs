795e65e388530fd8b482c6bc21fca95d096ff5ba
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-03-31 21:04:02 -0300

    Sword refactoring

--- Island Clicker/Assets/CODE/Scripts/Sword.cs
+++ Island Clicker/Assets/CODE/Scripts/Sword.cs
@@ -27,29 +27,32 @@ public class Sword : MonoBehaviour
     private int lastDamage;
     public void WoodUpgrade()
     {
-        if (Stats.Wood >= 10)
+        int woodAmount = 10;
+        if (Stats.Wood >= woodAmount)
         {
-            Stats.Wood -= 10;
+            Stats.Wood -= woodAmount;
             damage = 30;
-            uiManager.SpentMaterials(GameManager.EnemyTypes.Wood, 10);
+            uiManager.SpentMaterials(GameManager.EnemyTypes.Wood, woodAmount);
             woodUpgradeUI.SetActive(false);
             stoneUpgradeUI.SetActive(true);
         }
         else
         {
             //mostrar ui durante X segundos
-            ShowMessageCoroutine(woodRecipeUi, 3f);
+            StartCoroutine(ShowMessageCoroutine(woodRecipeUi, 0.75f));
             //Animation?
         }
     }
     public void StoneUpgrade()
     {
-        if (Stats.Wood >= 30 && Stats.Rock >= 4)
+        int rockAmount = 8;
+        int woodAmount = 30;
+        if (Stats.Wood >= woodAmount && Stats.Rock >= rockAmount)
         {
-            Stats.Rock -= 4;
-            Stats.Wood -= 30;
-            uiManager.SpentMaterials(GameManager.EnemyTypes.Wood, 30);
-            uiManager.SpentMaterials(GameManager.EnemyTypes.Rock, 4);
+            Stats.Rock -= rockAmount;
+            Stats.Wood -= woodAmount;
+            uiManager.SpentMaterials(GameManager.EnemyTypes.Wood, woodAmount);
+            uiManager.SpentMaterials(GameManager.EnemyTypes.Rock, rockAmount);
 
             damage = 60;
             stoneUpgradeUI.SetActive(false);
@@ -64,14 +67,18 @@ public class Sword : MonoBehaviour
     }
     public void WaterUpgrade()
     {
+        int rockAmount = 8;
+        int woodAmount = 30;
+        int waterAmount = 50;
+
         if (Stats.Wood >= 30 && Stats.Rock >= 8 && Stats.Water >= 50)
         {
-            Stats.Wood -= 30;
-            Stats.Rock -= 8;
-            Stats.Water -= 50;
-            uiManager.SpentMaterials(GameManager.EnemyTypes.Wood, 30);
-            uiManager.SpentMaterials(GameManager.EnemyTypes.Rock, 8);
-            uiManager.SpentMaterials(GameManager.EnemyTypes.Water, 50);
+            Stats.Rock -= rockAmount;
+            Stats.Wood -= woodAmount;
+            Stats.Water -= waterAmount;
+            uiManager.SpentMaterials(GameManager.EnemyTypes.Wood, woodAmount);
+            uiManager.SpentMaterials(GameManager.EnemyTypes.Rock, rockAmount);
+            uiManager.SpentMaterials(GameManager.EnemyTypes.Water, waterAmount);
             damage = 180;
             waterUpgradeUI.SetActive(false);
             holyWaterUpgradeUI.SetActive(true);
@@ -86,15 +93,18 @@ public class Sword : MonoBehaviour
     }
     public void HolyWaterUpgrade()
     {
+        int rockAmount = 16;
+        int woodAmount = 90;
+        int waterAmount = 225;
         //Check if you have the requisites
-        if (Stats.Wood >= 90 && Stats.Rock >= 16 && Stats.Water >= 225)
+        if (Stats.Wood >= woodAmount && Stats.Rock >= rockAmount && Stats.Water >= waterAmount)
         {
-            Stats.Wood -= 90;
-            Stats.Rock -= 16;
-            Stats.Water -= 225;
-            uiManager.SpentMaterials(GameManager.EnemyTypes.Wood, 90);
-            uiManager.SpentMaterials(GameManager.EnemyTypes.Rock, 16);
-            uiManager.SpentMaterials(GameManager.EnemyTypes.Water, 225);
+            Stats.Wood -= woodAmount;
+            Stats.Rock -= rockAmount;
+            Stats.Water -= waterAmount;
+            uiManager.SpentMaterials(GameManager.EnemyTypes.Wood, woodAmount);
+            uiManager.SpentMaterials(GameManager.EnemyTypes.Rock, rockAmount);
+            uiManager.SpentMaterials(GameManager.EnemyTypes.Water, waterAmount);
             damage = 540;
             holyWaterUpgradeUI.SetActive(false);
             crystalUpgradeUI.SetActive(true);
@@ -109,12 +119,15 @@ public class Sword : MonoBehaviour
     }
     public void CrystalUpgrade()
     {
-        if (Stats.Crystal >= 6 && Stats.Water >= 150)
+        int waterAmount = 150;
+        int crystalAmount = 6;
+
+        if (Stats.Crystal >= crystalAmount && Stats.Water >= waterAmount)
         {
-            Stats.Crystal -= 6;
-            Stats.Water -= 150;
-            uiManager.SpentMaterials(GameManager.EnemyTypes.Crystal, 6);
-            uiManager.SpentMaterials(GameManager.EnemyTypes.Water, 150);
+            Stats.Crystal -= crystalAmount;
+            Stats.Water -= waterAmount;
+            uiManager.SpentMaterials(GameManager.EnemyTypes.Crystal, crystalAmount);
+            uiManager.SpentMaterials(GameManager.EnemyTypes.Water, waterAmount);
 
 
             damage = 1080;
