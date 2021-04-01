c53338305fbbeaa55ea81016a7ff29f8fe7f7b74
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-03-31 23:54:55 -0300

    Statics reset on Main menu (Prevents wrong behaviour after dying and playing again)

---
+++ Island Clicker/Assets/CODE/Scripts/MainMenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    private void Start()
+    {
+
+        ResetAll();
+
+    }
+    public void Play()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Play");
+    }
+    private void ResetAll()
+    {
+        Stats.Crystal = 0;
+        Stats.Rock = 0;
+        Stats.Water = 0;
+        Stats.Wood = 0;
+        Stats.Experience = 0;
+
+        StaticReference.GameManager = null;
+        StaticReference.Sword = null;
+        StaticReference.Target = null;
+
+        Enemy.speedMultiplier = 1;
+    }
+}
